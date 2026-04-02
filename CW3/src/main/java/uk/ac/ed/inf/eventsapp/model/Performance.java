7c4e36f08c8d2dbe9839fc60f144806bf53b521c
King Jin <kingjin141319@gmail.com>
2026-04-02 19:22:08 +0100

    Feature/king cw3 task1 (#68)
    * add } to fix red error

--- CW3/src/main/java/uk/ac/ed/inf/eventsapp/model/Performance.java
+++ CW3/src/main/java/uk/ac/ed/inf/eventsapp/model/Performance.java
@@ -102,6 +102,8 @@ public class Performance {
 
   public void addNumTicketsSold(int numTickets) {
     numTicketsSold += numTickets;
+  }
+
   boolean isActive() {
     return status == PerformanceStatus.ACTIVE;
   }
